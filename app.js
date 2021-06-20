07bf22c23376c2d83c74e914a4cba545b226683a
arif <moarifur@gmail.com>
2021-06-20 18:55:29 +0600

    Project 04: Form Validation Successfully Done!

---
+++ app.js
@@ -0,0 +1,23 @@
+/* -------------------- function -------------------
+
+- Declarative function / Function declarations
+- Expressive function / Function expressions
+- Arrow function
+- IFFI
+- Annoymous Function
+- Callback function
+
+
+Assignment: Do addition / subtraction / multiplication / division with 
+- Function declarations
+- Function expressions
+- Arrow function
+
+*/ 
+
+function add(a, b) {
+    let result = a + b
+    return result    
+}
+
+console.log(add())
